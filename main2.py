8a92ef2c6815c0527fee364d8551bfd2f987028a
Aditya090202 <90269753+Aditya090202@users.noreply.github.com>
2021-09-09 12:01:01 -0400

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,9 @@
+name = "Aditya Mathur"
+school = "University at Buffalo"
+age = 19
+def name_age_diff():
+    if age > 18:
+        print("Yay you're an adult!")
+    else:
+        print("Not an adult yet, sorry!")    
+name_age_diff()
